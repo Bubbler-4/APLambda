0500bab83d70335c3e465c4e4a5c69c53afeb94a
Bubbler-4 <bubbler9903@gmail.com>
2020-03-17 01:04:41 +0000

    Setup ESLint for Typescript

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
-eslint src/ --fix
-mkdir -p /tmp/out
-rollup src/aplambda_ui.js -o /tmp/out/aplambda_bundle.js -c
-terser /tmp/out/aplambda_bundle.js -c -m -o docs/aplambda_bundle.min.js
+#eslint src/ --fix
+#mkdir -p /tmp/out
+#rollup src/aplambda_ui.js -o /tmp/out/aplambda_bundle.js -c
+#terser /tmp/out/aplambda_bundle.js -c -m -o docs/aplambda_bundle.min.js
+
+# Remove previous files
+rm -rf docs/*
+
+# Build the page
+parcel build src/index.html -d docs
+
+# Replace absolute path with relative one, so that it works on Gitpod AND GH pages
+sed -i -e 's/src="\//src="/g' docs/index.html
